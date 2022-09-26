9f91917e096d3e4c8b57ac8d89ca465d55b03328
bigbanglwb <bigbanglwb@163.com>
2022-09-26 11:13:38 +0800

    queryTicketLogTime

--- build.sh
+++ build.sh
@@ -2,12 +2,35 @@
 cd ~/train-ticket
 git pull
 mvn clean package -Dmaven.test.skip=true
-cd ts-basic-service
-docker build -t bigbanglwb/ts-basic-service:1.0.0 .
-cd ../ts-station-service
-docker build -t bigbanglwb/ts-station-service:1.0.0 .
-kubectl delete pod `kubectl get pods |grep ts-basic-service | awk '{print $1}'`
-kubectl delete pod `kubectl get pods |grep ts-station-service | awk '{print $1}'`
 
-# kubectl logs -f `kubectl get pods |grep ts-basic-service | awk '{print $1}'`
-# kubectl logs -f `kubectl get pods |grep ts-station-service | awk '{print $1}'`
+
+#svc_list=("travel" "basic" "station" "train" "route" "price" "seat" "order" "config")
+## shellcheck disable=SC2068
+#for svc in ${svc_list[@]}
+#do
+#        # kubectl delete deployment ts-${svc}-service
+#        # kubectl delete svc ts-${svc}-service
+#        cd ~/train-ticket/ts-${svc}-service || exit
+#        docker build -t bigbanglwb/ts-${svc}-service:1.0.0 .
+#        # shellcheck disable=SC2046
+#        kubectl delete pod `kubectl get pods |grep ts-${svc}-service | awk '{print $1}'`
+#done
+#
+## kubectl apply -f /home/liwenbo/train-ticket/deployment/kubernetes-manifests/quickstart-k8s/yamls/deploy.yaml
+## kubectl apply -f /home/liwenbo/train-ticket/deployment/kubernetes-manifests/quickstart-k8s/yamls/svc.yaml
+
+
+svc_list=("travel" "basic" "station")
+# shellcheck disable=SC2068
+for svc in ${svc_list[@]}
+do
+        kubectl delete deployment ts-"${svc}"-service
+        kubectl delete svc ts-"${svc}"-service
+        cd ~/train-ticket/ts-"${svc}"-service || exit
+        # shellcheck disable=SC2086
+        docker build -t bigbanglwb/ts-${svc}-service:1.0.0 .
+#        kubectl delete pod `kubectl get pods |grep ts-${svc}-service | awk '{print $1}'`
+done
+
+ kubectl apply -f /home/liwenbo/train-ticket/deployment/kubernetes-manifests/quickstart-k8s/yamls/deploy.yaml
+ kubectl apply -f /home/liwenbo/train-ticket/deployment/kubernetes-manifests/quickstart-k8s/yamls/svc.yaml
