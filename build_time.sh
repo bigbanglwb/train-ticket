c02095640430ec5cbac537dc57e11f64e7b79e8d
bigbanglwb <bigbanglwb@163.com>
2022-09-28 15:09:15 +0800

    time

--- build_time.sh
+++ build_time.sh
@@ -3,6 +3,6 @@ cd ~/train-ticket
 git pull
 mvn  clean package -Dmaven.test.skip=true -pl ts-time-service -am
 cd ~/train-ticket/ts-time-service || exit
-docker build -t codewisdom/ts-time-service:2.0.0 .
+docker build -t codewisdom/ts-time-service:3.0.0 .
 # shellcheck disable=SC2046
-kubectl patch pod ts-time-service-7b8c4964fd-hgjlg   -p '{"spec":{"containers":[{"name": "ts-time-service","image": "codewisdom/ts-time-service:2.0.0"}]}}'
+kubectl patch pod ts-time-service-6cff58dc4c-6q68r   -p '{"spec":{"containers":[{"name": "ts-time-service","image": "codewisdom/ts-time-service:3.0.0"}]}}'
