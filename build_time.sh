7cbad19a25b7e9d8e3487d60b542619625419e0a
bigbanglwb <bigbanglwb@163.com>
2022-09-28 15:06:43 +0800

    time

--- build_time.sh
+++ build_time.sh
@@ -3,18 +3,6 @@ cd ~/train-ticket
 git pull
 mvn  clean package -Dmaven.test.skip=true -pl ts-time-service -am
 cd ~/train-ticket/ts-time-service || exit
-docker build -t codewisdom/ts-time-service:1.0.0 .
+docker build -t codewisdom/ts-time-service:2.0.0 .
 # shellcheck disable=SC2046
-kubectl delete -f ./deploy.yaml
-kubectl apply -f ./deploy.yaml
-
-
-
-#!/bin/bash
-cd ~/train-ticket
-git pull
-mvn  clean package -Dmaven.test.skip=true -pl ts-rebook-service -am
-cd ~/train-ticket/ts-rebook-service || exit
-docker build -t codewisdom/ts-rebook-service:1.0.0 .
-# shellcheck disable=SC2046
-kubectl delete pod `kubectl get pods |grep ts-rebook-service | awk '{print $1}'`
+kubectl patch pod ts-time-service-7b8c4964fd-hgjlg   -p '{"spec":{"containers":[{"name": "ts-time-service","image": "codewisdom/ts-time-service:2.0.0"}]}}'
