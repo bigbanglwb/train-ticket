986c2a60bc72936f259a4a52c07f22931c93f815
bigbanglwb <bigbanglwb@163.com>
2022-10-14 15:27:33 +0800

    time

--- rebuild_service.sh
+++ rebuild_service.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 service_name=$1
-cd ~/train-ticket
-mvn  clean package -Dmaven.test.skip=true -pl ts-basic-service -am
-cd ~/train-ticket/ts-basic-service || exit
-docker build -t codewisdom/ts-basic-service:1.0.0 .
-kubectl delete pod `kubectl get pods |grep ts-basic-service | awk '{print $1}'`
+echo ${service_name}
+#cd ~/train-ticket
+#mvn  clean package -Dmaven.test.skip=true -pl ts-basic-service -am
+#cd ~/train-ticket/ts-basic-service || exit
+#docker build -t codewisdom/ts-basic-service:1.0.0 .
+#kubectl delete pod `kubectl get pods |grep ts-basic-service | awk '{print $1}'`
 
